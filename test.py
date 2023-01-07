e9055f83b0f0b296214c0834837a77cdee020fe9
Bo <bo.van.laarhoven@gmail.com>
2023-01-08 00:07:46 +0100

    ok

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
 import pyautogui
-
-screenWidth, screenHeight = pyautogui.size()
-currentMouseX, currentMouseY = pyautogui.position()
-
-while True:
-    if pyautogui.position() == (0, 0):
-        print("Mouse is at (0, 0).")
-    else:
-        pyautogui.moveTo()
+for pos in pyautogui.locateAllOnScreen('window.png')
+    print(pos)
