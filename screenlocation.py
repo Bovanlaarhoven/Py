c7994b9407b8730146b8b8023363e5127d2c74a5
Bo <bo.van.laarhoven@gmail.com>
2023-01-13 00:01:52 +0100

    ok

--- screenlocation.py
+++ screenlocation.py
@@ -1,7 +1,7 @@
 import pyautogui, sys
 try:
     while True:
-        x, y = pyautogui.position()
+        x, y = pyautogui.displayMousePosition()
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
