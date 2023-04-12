914b9746a38bbfc4d71047d5cf5346a46af76a1e
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-12 23:20:46 +0200

    Create send.py

---
+++ send.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://camo.githubusercontent.com/77e56b1873f7b8d1f5068cef42dbe33055583e7d56b94e30f3021732222f536b/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d726f626f626f32303232266c6162656c3d50726f66696c65253230766965777326636f6c6f723d306537356236267374796c653d666c6174'
+
+request_count = 0
+
+while True:
+    response = requests.get(url)
+    request_count += 1
+    print(f"Requests sent: {request_count}")
+    # process the response here as needed
