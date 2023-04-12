5035a20ea03a0a901f55c52c9b048012dbc2648a
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-12 23:50:13 +0200

    Update send2.py

--- send2.py
+++ send2.py
@@ -1,8 +1,16 @@
 import requests
 import time
 import concurrent.futures
+import random
 
 url = 'https://visitcount.itsvg.in/api?id=Robobo2022&icon=0&color=0'
+
+proxies = [
+    'http://10.10.1.10:3128',
+    'https://10.10.1.10:1080',
+    'http://10.10.1.11:3128',
+    'https://10.10.1.11:1080',
+]
 
 request_count = 0
 
@@ -10,7 +18,8 @@ def send_requests():
     global request_count
     while True:
         try:
-            response = requests.get(url)
+            proxy = {'https': proxies[random.randint(0, len(proxies)-1)]}
+            response = requests.get(url, proxies=proxy, timeout=10)
             if response.status_code == 200:
                 request_count += 1
                 print(f"\rRequests sent: {request_count}", end='')
@@ -19,7 +28,7 @@ def send_requests():
                 time.sleep(60)
             else:
                 print(f"\rError {response.status_code} occurred", end='')
-        except:
+        except requests.exceptions.RequestException:
             print("\rError occurred while making request", end='')
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
