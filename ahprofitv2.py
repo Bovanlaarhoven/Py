71a1a8de466b86a4d0b074db60ce0730fdbe7e2a
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-05-21 21:20:58 +0200

    Update ahprofitv2.py

--- ahprofitv2.py
+++ ahprofitv2.py
@@ -1,8 +1,8 @@
+import asyncio
 import re
 import os
-import certifi
-os.environ['SSL_CERT_FILE'] = certifi.where()
-import subprocess
+op = os.name == 'nt'
+if op: import winsound
 from concurrent.futures import ThreadPoolExecutor
 from timeit import default_timer
 import time
@@ -11,8 +11,6 @@ import pandas as pd
 import requests
 
 from plyer import notification
-import asyncio
-import aiohttp
 
 c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0")
 resp = c.json()
@@ -23,16 +21,7 @@ results = []
 prices = {}
 
 # stuff to remove
-REFORGES = [" ✦", "⚚ ", " ✪", "✪", "Stiff ", "Lucky ", "Jerry's ", "Dirty ", "Fabled ", "Suspicious ", "Gilded ", "Warped ",
-            "Withered ", "Bulky ", "Stellar ", "Heated ", "Ambered ", "Fruitful ", "Magnetic ", "Fleet ", "Mithraic ",
-            "Auspicious ", "Refined ", "Headstrong ", "Precise ", "Spiritual ", "Moil ", "Blessed ", "Toil ", "Bountiful ",
-            "Candied ", "Submerged ", "Reinforced ", "Cubic ", "Warped ", "Undead ", "Ridiculous ", "Necrotic ", "Spiked ",
-            "Jaded ", "Loving ", "Perfect ", "Renowned ", "Giant ", "Empowered ", "Ancient ", "Sweet ", "Silky ", "Bloody ",
-            "Shaded ", "Gentle ", "Odd ", "Fast ", "Fair ", "Epic ", "Sharp ", "Heroic ", "Spicy ", "Legendary ", "Deadly ",
-            "Fine ", "Grand ", "Hasty ", "Neat ", "Rapid ", "Unreal ", "Awkward ", "Rich ", "Clean ", "Fierce ", "Heavy ",
-            "Light ", "Mythic ", "Pure ", "Smart ", "Titanic ", "Wise ", "Bizarre ", "Itchy ", "Ominous ", "Pleasant ",
-            "Pretty ", "Shiny ", "Simple ", "Strange ", "Vivid ", "Godly ", "Demonic ", "Forceful ", "Hurtful ", "Keen ",
-            "Strong ", "Superior ", "Unpleasant ", "Zealous "]
+REFORGES = [" ✦", "⚚ ", " ✪", "✪", "Stiff ", "Lucky ", "Jerry's ", "Dirty ", "Fabled ", "Suspicious ", "Gilded ", "Warped ", "Withered ", "Bulky ", "Stellar ", "Heated ", "Ambered ", "Fruitful ", "Magnetic ", "Fleet ", "Mithraic ", "Auspicious ", "Refined ", "Headstrong ", "Precise ", "Spiritual ", "Moil ", "Blessed ", "Toil ", "Bountiful ", "Candied ", "Submerged ", "Reinforced ", "Cubic ", "Warped ", "Undead ", "Ridiculous ", "Necrotic ", "Spiked ", "Jaded ", "Loving ", "Perfect ", "Renowned ", "Giant ", "Empowered ", "Ancient ", "Sweet ", "Silky ", "Bloody ", "Shaded ", "Gentle ", "Odd ", "Fast ", "Fair ", "Epic ", "Sharp ", "Heroic ", "Spicy ", "Legendary ", "Deadly ", "Fine ", "Grand ", "Hasty ", "Neat ", "Rapid ", "Unreal ", "Awkward ", "Rich ", "Clean ", "Fierce ", "Heavy ", "Light ", "Mythic ", "Pure ", "Smart ", "Titanic ", "Wise ", "Bizarre ", "Itchy ", "Ominous ", "Pleasant ", "Pretty ", "Shiny ", "Simple ", "Strange ", "Vivid ", "Godly ", "Demonic ", "Forceful ", "Hurtful ", "Keen ", "Strong ", "Superior ", "Unpleasant ", "Zealous "]
 
 # Constant for the lowest priced item you want to be shown to you; feel free to change this
 LOWEST_PRICE = 5
@@ -41,64 +30,112 @@ LOWEST_PRICE = 5
 NOTIFY = False
 
 # Constant for the lowest percent difference you want to be shown to you; feel free to change this
-LOWEST_PERCENT_MARGIN = 1 / 2
+LOWEST_PERCENT_MARGIN = 1/2
 
 START_TIME = default_timer()
 
-async def fetch(session, page):
+def fetch(session, page):
     global toppage
     base_url = "https://api.hypixel.net/skyblock/auctions?page="
-    async with session.get(base_url + page) as response:
+    with session.get(base_url + page) as response:
         # puts response in a dict
-        data = await response.json()
+        data = response.json()
         toppage = data['totalPages']
         if data['success']:
             toppage = data['totalPages']
             for auction in data['auctions']:
-                if not auction['claimed'] and auction['bin'] == True and not "Furniture" in auction[
-                    "item_lore"]:  # if the auction isn't a) claimed and b) not a BIN, then continue
-                    index = auction['item_name']
-                    for r in REFORGES:
-                        index = index.replace(r, "")
-                    if index not in prices:
-                        prices[index] = auction['starting_bid']
+                if not auction['claimed'] and auction['bin'] == True and not "Furniture" in auction["item_lore"]: # if the auction isn't a) claimed and is b) BIN
+                    # removes level if it's a pet, also 
+                    index = re.sub("\[[^\]]*\]", "", auction['item_name']) + auction['tier']
+                    # removes reforges and other yucky characters
+                    for reforge in REFORGES: index = index.replace(reforge, "")
+                    # if the current item already has a price in the prices map, the price is updated
+                    if index in prices:
+                        if prices[index][0] > auction['starting_bid']:
+                            prices[index][1] = prices[index][0]
+                            prices[index][0] = auction['starting_bid']
+                        elif prices[index][1] > auction['starting_bid']:
+                            prices[index][1] = auction['starting_bid']
+                    # otherwise, it's added to the prices map
                     else:
-                        prices[index] += auction['starting_bid']
-
+                        prices[index] = [auction['starting_bid'], float("inf")]
+                        
+                    # if the auction fits in some parameters
+                    if prices[index][1] > LOWEST_PRICE and prices[index][0]/prices[index][1] < LOWEST_PERCENT_MARGIN and auction['start']+60000 > now:
+                        results.append([auction['uuid'], auction['item_name'], auction['starting_bid'], index])
+        return data
 
 async def get_data_asynchronous():
-    tasks = []
-    pages = list(range(0, toppage))
-    async with aiohttp.ClientSession() as session:
-        for page in pages:
-            task = asyncio.ensure_future(fetch(session, str(page)))
-            tasks.append(task)
-        responses = await asyncio.gather(*tasks)
+    # puts all the page strings
+    pages = [str(x) for x in range(toppage)]
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        with requests.Session() as session:
+            loop = asyncio.get_event_loop()
+            START_TIME = default_timer()
+            tasks = [
+                loop.run_in_executor(
+                    executor,
+                    fetch,
+                    *(session, page) # Allows us to pass in multiple arguments to `fetch`
+                )
+                # runs for every page
+                for page in pages if int(page) < toppage
+            ]
+            for response in await asyncio.gather(*tasks):
+                pass
 
-        if prices:
-            for index in prices:
-                price = prices[index]
-                average = price / toppage
-                margin = round(price / average, 3)
-                if margin > LOWEST_PERCENT_MARGIN and price > LOWEST_PRICE:
-                    results.append((index, price, margin))
-            results.sort(key=lambda x: x[1])
-            for result in results:
-                if result[1] < 500000:
-                    message = "Lowest BIN: {:,}\nSecond Lowest: {:,}".format(result[0][2], result[1])
-                    if NOTIFY:
-                        notification.notify(
-                            title="AH Notification",
-                            message=message,
-                            timeout=5,
-                        )
-                    else:
-                        print(message)
+def main():
+    # Resets variables
+    global results, prices, START_TIME
+    START_TIME = default_timer()
+    results = []
+    prices = {}
+    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    future = asyncio.ensure_future(get_data_asynchronous())
+    loop.run_until_complete(future)
+    
+    # Makes sure all the results are still up to date
+    if len(results): results = [[entry, prices[entry[3]][1]] for entry in results if (entry[2] > LOWEST_PRICE and prices[entry[3]][1] != float('inf') and prices[entry[3]][0] == entry[2] and prices[entry[3]][0]/prices[entry[3]][1] < LOWEST_PERCENT_MARGIN)]
+    
+    if len(results): # if there's results to print
 
+        if NOTIFY: 
+            notification.notify(
+                title = max(results, key=lambda entry:entry[1])[0][1],
+                message = "Lowest BIN: " + f'{max(results, key=lambda entry:entry[1])[0][2]:,}' + "\nSecond Lowest: " + f'{max(results, key=lambda entry:entry[1])[1]:,}',
+                app_icon = None,
+                timeout = 4,
+            )
+        
+        df=pd.DataFrame(['/viewauction ' + str(max(results, key=lambda entry:entry[1])[0][0])])
+        df.to_clipboard(index=False,header=False) # copies most valuable auction to clipboard (usually just the only auction cuz very uncommon for there to be multiple
+        
+        done = default_timer() - START_TIME
+        if op: winsound.Beep(500, 500) # emits a frequency 500hz, for 500ms
+        for result in results:
+            print("Auction UUID: " + str(result[0][0]) + " | Item Name: " + str(result[0][1]) + " | Item price: {:,}".format(result[0][2]), " | Second lowest BIN: {:,}".format(result[1]) + " | Time to refresh AH: " + str(round(done, 2)))
+        print("\nLooking for auctions...")
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        await get_data_asynchronous()
+print("Looking for auctions...")
+main()
 
-if __name__ == '__main__':
-    asyncio.run(main())
+def dostuff():
+    global now, toppage
+
+    # if 60 seconds have passed since the last update
+    if time.time()*1000 > now + 60000:
+        prevnow = now
+        now = float('inf')
+        c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0").json()
+        if c['lastUpdated'] != prevnow:
+            now = c['lastUpdated']
+            toppage = c['totalPages']
+            main()
+        else:
+            now = prevnow
+    time.sleep(0.25)
+
+while True:
+    dostuff()
