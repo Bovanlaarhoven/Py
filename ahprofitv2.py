e7e52ad91815861e8de5f760a1adc6e9219a4f19
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-05-21 21:52:59 +0200

    Update ahprofitv2.py

--- ahprofitv2.py
+++ ahprofitv2.py
@@ -9,7 +9,7 @@ import time
 import pandas as pd
 import requests
 
-from plyer import notification
+from pync import Notifier
 
 c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0")
 resp = c.json()
@@ -19,16 +19,10 @@ toppage = resp['totalPages']
 results = []
 prices = {}
 
-# stuff to remove
 REFORGES = [" ✦", "⚚ ", " ✪", "✪", "Stiff ", "Lucky ", "Jerry's ", "Dirty ", "Fabled ", "Suspicious ", "Gilded ", "Warped ", "Withered ", "Bulky ", "Stellar ", "Heated ", "Ambered ", "Fruitful ", "Magnetic ", "Fleet ", "Mithraic ", "Auspicious ", "Refined ", "Headstrong ", "Precise ", "Spiritual ", "Moil ", "Blessed ", "Toil ", "Bountiful ", "Candied ", "Submerged ", "Reinforced ", "Cubic ", "Warped ", "Undead ", "Ridiculous ", "Necrotic ", "Spiked ", "Jaded ", "Loving ", "Perfect ", "Renowned ", "Giant ", "Empowered ", "Ancient ", "Sweet ", "Silky ", "Bloody ", "Shaded ", "Gentle ", "Odd ", "Fast ", "Fair ", "Epic ", "Sharp ", "Heroic ", "Spicy ", "Legendary ", "Deadly ", "Fine ", "Grand ", "Hasty ", "Neat ", "Rapid ", "Unreal ", "Awkward ", "Rich ", "Clean ", "Fierce ", "Heavy ", "Light ", "Mythic ", "Pure ", "Smart ", "Titanic ", "Wise ", "Bizarre ", "Itchy ", "Ominous ", "Pleasant ", "Pretty ", "Shiny ", "Simple ", "Strange ", "Vivid ", "Godly ", "Demonic ", "Forceful ", "Hurtful ", "Keen ", "Strong ", "Superior ", "Unpleasant ", "Zealous "]
 
-# Constant for the lowest priced item you want to be shown to you; feel free to change this
 LOWEST_PRICE = 5
-
-# Constant to turn on/off desktop notifications
 NOTIFY = False
-
-# Constant for the lowest percent difference you want to be shown to you; feel free to change this
 LOWEST_PERCENT_MARGIN = 1/2
 
 START_TIME = default_timer()
@@ -37,35 +31,28 @@ def fetch(session, page):
     global toppage
     base_url = "https://api.hypixel.net/skyblock/auctions?page="
     with session.get(base_url + page) as response:
-        # puts response in a dict
         data = response.json()
         toppage = data['totalPages']
         if data['success']:
             toppage = data['totalPages']
             for auction in data['auctions']:
-                if not auction['claimed'] and auction['bin'] == True and not "Furniture" in auction["item_lore"]: # if the auction isn't a) claimed and is b) BIN
-                    # removes level if it's a pet, also 
+                if not auction['claimed'] and auction['bin'] == True and not "Furniture" in auction["item_lore"]:
                     index = re.sub("\[[^\]]*\]", "", auction['item_name']) + auction['tier']
-                    # removes reforges and other yucky characters
                     for reforge in REFORGES: index = index.replace(reforge, "")
-                    # if the current item already has a price in the prices map, the price is updated
                     if index in prices:
                         if prices[index][0] > auction['starting_bid']:
                             prices[index][1] = prices[index][0]
                             prices[index][0] = auction['starting_bid']
                         elif prices[index][1] > auction['starting_bid']:
                             prices[index][1] = auction['starting_bid']
-                    # otherwise, it's added to the prices map
                     else:
                         prices[index] = [auction['starting_bid'], float("inf")]
                         
-                    # if the auction fits in some parameters
                     if prices[index][1] > LOWEST_PRICE and prices[index][0]/prices[index][1] < LOWEST_PERCENT_MARGIN and auction['start']+60000 > now:
                         results.append([auction['uuid'], auction['item_name'], auction['starting_bid'], index])
         return data
 
 async def get_data_asynchronous():
-    # puts all the page strings
     pages = [str(x) for x in range(toppage)]
     with ThreadPoolExecutor(max_workers=10) as executor:
         with requests.Session() as session:
@@ -75,16 +62,14 @@ async def get_data_asynchronous():
                 loop.run_in_executor(
                     executor,
                     fetch,
-                    *(session, page) # Allows us to pass in multiple arguments to `fetch`
+                    *(session, page)
                 )
-                # runs for every page
                 for page in pages if int(page) < toppage
             ]
             for response in await asyncio.gather(*tasks):
                 pass
 
 def main():
-    # Resets variables
     global results, prices, START_TIME
     START_TIME = default_timer()
     results = []
@@ -95,32 +80,30 @@ def main():
     future = asyncio.ensure_future(get_data_asynchronous())
     loop.run_until_complete(future)
     
-    # Makes sure all the results are still up to date
     if len(results): results = [[entry, prices[entry[3]][1]] for entry in results if (entry[2] > LOWEST_PRICE and prices[entry[3]][1] != float('inf') and prices[entry[3]][0] == entry[2] and prices[entry[3]][0]/prices[entry[3]][1] < LOWEST_PERCENT_MARGIN)]
     
-    if len(results): # if there's results to print
+    if len(results):
 
         if NOTIFY: 
-            notification.notify(
-                title = max(results, key=lambda entry:entry[1])[0][1],
-                message = "Lowest BIN: " + f'{max(results, key=lambda entry:entry[1])[0][2]:,}' + "\nSecond Lowest: " + f'{max(results, key=lambda entry:entry[1])[1]:,}',
-                app_icon = None,
-                timeout = 4,
+            Notifier.notify(
+                'Notification Title',
+                title=max(results, key=lambda entry: entry[1])[0][1],
+                subtitle="Lowest BIN: " + f'{max(results, key=lambda entry: entry[1])[0][2]:,}' + "\nSecond Lowest: " + f'{max(results, key=lambda entry: entry[1])[1]:,}',
+                sound='default',
             )
         
-        df=pd.DataFrame(['/viewauction ' + str(max(results, key=lambda entry:entry[1])[0][0])])
-        df.to_clipboard(index=False,header=False) # copies most valuable auction to clipboard (usually just the only auction cuz very uncommon for there to be multiple
+        df = pd.DataFrame(['/viewauction ' + str(max(results, key=lambda entry: entry[1])[0][0])])
+        df.to_clipboard(index=False, header=False)
         
         done = default_timer() - START_TIME
-        play_sound()  # Play the sound
+        play_sound()
         for result in results:
             print("Auction UUID: " + str(result[0][0]) + " | Item Name: " + str(result[0][1]) + " | Item price: {:,}".format(result[0][2]), " | Second lowest BIN: {:,}".format(result[1]) + " | Time to refresh AH: " + str(round(done, 2)))
         print("\nLooking for auctions...")
 
 def play_sound():
-    # Check the OS and play sound accordingly
     if os.name == 'nt':
-        winsound.Beep(500, 500) # emits a frequency 500hz, for 500ms
+        winsound.Beep(500, 500)
     elif os.name == 'posix':
         subprocess.call(["afplay", "/System/Library/Sounds/Glass.aiff"])
 
@@ -130,8 +113,7 @@ main()
 def dostuff():
     global now, toppage
 
-    # if 60 seconds have passed since the last update
-    if time.time()*1000 > now + 60000:
+    if time.time() * 1000 > now + 60000:
         prevnow = now
         now = float('inf')
         c = requests.get("https://api.hypixel.net/skyblock/auctions?page=0").json()
@@ -141,7 +123,7 @@ def dostuff():
             main()
         else:
             now = prevnow
-    time.sleep(0.25)
+    time.sleep(0.1)
 
 while True:
     dostuff()
