a46d9bb68753b70d7cdc124ef76b639559b661d9
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-08-30 19:55:33 +0200

    Update walk.py

--- walk.py
+++ walk.py
@@ -1,10 +1,12 @@
 import pyautogui
 import time
+import random
 
 def hold_key(key, duration):
     start_time = time.time()
     while time.time() - start_time < duration:
         pyautogui.keyDown(key)
+        time.sleep(0.01)  # A small delay to prevent spamming key presses
 
 if __name__ == "__main__":
     print("Press 'Ctrl+C' to exit the script.")
@@ -12,6 +14,16 @@ if __name__ == "__main__":
         if pyautogui.keyDown('ctrl') and pyautogui.press('c'):
             print("Exiting the script.")
             break
+        
+        time.sleep(1)  # Introduce a 1-second delay
 
-        hold_key('d', 250)
-        hold_key('a', 250)
+        random_duration = random.uniform(251, 253)
+        pyautogui.mouseDown(button='left')
+        
+        hold_key('d', random_duration)
+        hold_key('a', random_duration)
+        hold_key('d', random_duration)
+        hold_key('a', random_duration)
+        hold_key('d', random_duration)
+        
+        pyautogui.mouseUp(button='left')
