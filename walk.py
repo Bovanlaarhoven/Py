8f7058949953b91c8ecb61035de5e65134d7a56d
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-08-30 19:49:24 +0200

    Update walk.py

--- walk.py
+++ walk.py
@@ -5,8 +5,6 @@ def hold_key(key, duration):
     start_time = time.time()
     while time.time() - start_time < duration:
         pyautogui.keyDown(key)
-        time.sleep(0.01)
-        pyautogui.keyUp(key)
 
 if __name__ == "__main__":
     print("Press 'Ctrl+C' to exit the script.")
@@ -15,7 +13,5 @@ if __name__ == "__main__":
             print("Exiting the script.")
             break
 
-        pyautogui.mouseDown()  # Hold left mouse button
-        hold_key('d', 250)     # Hold 'd' key for 250 seconds
-        hold_key('a', 250)     # Hold 'a' key for 250 seconds
-        pyautogui.mouseUp()    # Release left mouse button
+        hold_key('d', 250)
+        hold_key('a', 250)
