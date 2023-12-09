34e7f5a85fef596a5e92defaedef901696b6a507
bovanlaarhoven <bo.van.laarhoven@gmail.com>
2023-12-09 12:58:35 +0100

    Update smolrequest.py

--- smolrequest.py
+++ smolrequest.py
@@ -12,7 +12,7 @@ SUCCESS_COUNT = 0
 def send_request(session, progress_bar):
     global SUCCESS_COUNT
     try:
-        response = session.get(link, timeout=5)  # Add timeout to avoid long waits
+        response = session.get(link, timeout=10)
         response.raise_for_status()
         SUCCESS_COUNT += 1
         progress_bar.update(1)
@@ -21,7 +21,7 @@ def send_request(session, progress_bar):
 
 def main():
     session = requests.Session()
-    session.headers.update({'User-Agent': ua.random})  # Set User-Agent globally
+    session.headers.update({'User-Agent': ua.random})  
 
     progress_bar = tqdm(total=amount_request, desc="Sending Requests", unit="req")
     threads = [threading.Thread(target=send_request, args=(session, progress_bar)) for _ in range(amount_request)]
